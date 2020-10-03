b6df089b3e4c8ca75cbc720b1309474fbb557b37
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-03 22:28:09 +0530

    Srings

---
+++ 02.Strings/src/StringM.java
@@ -0,0 +1,11 @@
+
+public class StringM {
+
+		public static void main(String[] args) {
+		String firstName = "Hello ";
+	    String lastName = "World";
+	    String fullName = firstName + lastName;
+	    System.out.println(fullName);  
+	}
+
+}
