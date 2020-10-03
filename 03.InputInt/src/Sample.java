78511bbce0cdf5caf0a091694f3286eca1e8f058
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-03 22:29:01 +0530

    Input Data

---
+++ 03.InputInt/src/Sample.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Sample {
+
+
+	public static void main(String[] args) 
+	{
+		Scanner x = new Scanner(System.in);
+		
+        System.out.print("Enter a number: ");
+
+         
+        int number = x.nextInt();
+
+ 
+        System.out.println("You entered: " + number);
+	}
+
+}
