74296a110d7b47db8cdf6e8d9f19b0f08604642f
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-23 21:06:12 +0530

    Multilevel Inheritance

---
+++ 12.Multilevel Inheritance/src/Class_C.java
@@ -0,0 +1,12 @@
+class Class_C extends Class_B
+{
+	public static void main(String[] args) 
+	{
+		 System.out.println("Class C Content"); 
+		 Class_C obj = new Class_C(); 
+		 obj.function_a(); 
+		 obj.function_b(); 
+		 obj.function_a(); 
+	}
+
+}
