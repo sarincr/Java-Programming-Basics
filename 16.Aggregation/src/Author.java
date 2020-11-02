5e4c6519889f4da78e019595aca01d305a543e73
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-11-02 10:08:22 +0530

    Aggregation

---
+++ 16.Aggregation/src/Author.java
@@ -0,0 +1,15 @@
+
+public class Author {
+	  String authorName;
+	  int age;
+	  String place;
+
+	  // Author class constructor
+	  Author(String name, int age, String place)
+	  {
+	    this.authorName = name;
+	    this.age = age;
+	    this.place = place;
+	  }
+
+}
